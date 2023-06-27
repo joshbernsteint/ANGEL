c99a988839cc0748ee8478e3f76f415f40d40976
Joshua Bernstein <joshuatbernstein@gmail.com>
2023-06-27 16:56:04 -0400

    bug fixes on setup file

--- YT_Server.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-start cmd /c "cd D:/Coding/YouTube_Downloader/server && npm run dev"
-cd D:/Coding/YouTube_Downloader/client && npm run start
-
-@REM DON'T USE THIS
--- server/win_setup.cpp
+++ server/win_setup.cpp
@@ -1,34 +1,42 @@
+/*
+
+Joshua Bernstein
+6/27/2023
+
+*/
 #include <windows.h>
 #include <stdio.h>
 #include <tchar.h>
-#include <iostream>
 
-
+/**
+ * Creates a child process
+ * @param cmd: Path to the executable
+ * @param args: Command line arguments to the executable
+ * @return Integer value representing if the process creation was a success(0) or failure(-1)
+*/
 int makeProcess(char* cmd,char* args ,STARTUPINFO &si, PROCESS_INFORMATION &pi){
     ZeroMemory( &si, sizeof(si) );
     si.cb = sizeof(si);
     ZeroMemory( &pi, sizeof(pi) );
 
-    if( !CreateProcess( cmd,   // No module name (use command line)
-        args,        // Command line
-        NULL,           // Process handle not inheritable
-        NULL,           // Thread handle not inheritable
-        FALSE,          // Set handle inheritance to FALSE
-        CREATE_NO_WINDOW,              // No creation flags
-        NULL,           // Use parent's environment block
-        NULL,           // Use parent's starting directory 
-        &si,            // Pointer to STARTUPINFO structure
-        &pi )           // Pointer to PROCESS_INFORMATION structure
-    )
+    if(!CreateProcess(cmd, args, NULL, NULL, 0, CREATE_NO_WINDOW, NULL, NULL, &si, &pi))
     {
-        printf( "CreateProcess failed (%d): %s.\n", GetLastError(),cmd );
+        //If there was an error in process creation
+        printf("Error: makeProcess failed: %s %s returned an error.\n",cmd, args);
         return -1;
     }
     else{
-        return 0;
+        return 0;//The proccess was made successfully
     }
 }
 
+/**
+ * Fills an empty array of chars with the values of a cha* cosnt
+ * @param str: A const char*
+ * @param target: A pointer to an array of chars
+ * @param len: The size of the array of chars
+ * @return An integer representing if filling the array was successful(0) or not(-1)
+*/
 int fillArray(const char* str,char* target,size_t len){
     size_t str_length = strlen(str)+1;
     if(str_length > len){
@@ -46,40 +54,28 @@ int _tmain( int argc, TCHAR *argv[] )
     STARTUPINFO server_si;
     PROCESS_INFORMATION server_pi;
 
-
     STARTUPINFO app_si;
     PROCESS_INFORMATION app_pi;
 
     bool windowOpened = true;
-
     HWND windowReturn;
-    bool firstTime = false;
-
-    // char app_call[32] = {'.','/','e','l','e','c','t','r','o','n','-','a','p','p','.','e','x','e','\0'};
-    // char app_call[32] = {'e','l','e','c','t','r','o','n','-','a','p','p','.','e','x','e','\0'};
-    // char server_call[64] = {'n','o','d','e',' ','.','/','y','t','_','s','e','r','v','e','r','/','s','e','r','v','e','r','\0'};
 
     const char* app_c = "./electron-app.exe";
     const char* server_c = "C:/Program Files/nodejs/node.exe";
     const char* server_a = " yt_server/server";
 
+    const char* window_name = "__PLACEHOLDER__";
 
-
-    char app_call[64];
+    char app_call[32];
     char server_call[64];
-    char server_args[64];
+    char server_args[32];
 
-    fillArray(app_c,app_call,64);
-    printf("%s\n",app_call);
-
+    fillArray(app_c,app_call,32);
     fillArray(server_c,server_call,64);
     fillArray(server_a,server_args,32);
-    printf("%s\n",server_call);
-    printf("%s\n",server_args);
-    
 
-
-    // Start the app process. 
+    FreeConsole();
+    // Start the processes. 
     printf("Starting Processes\n");
     makeProcess(app_call,NULL,app_si,app_pi);
     makeProcess(server_call,server_args,server_si,server_pi);
@@ -88,10 +84,9 @@ int _tmain( int argc, TCHAR *argv[] )
     
 
     // Wait until child process exits.
-    FreeConsole();
     while(windowOpened){
-        WaitForSingleObject( app_pi.hProcess, INFINITE );
-        windowReturn = FindWindow(NULL,"__PLACEHOLDER__");
+        WaitForSingleObject( app_pi.hProcess, INFINITE ); //Wait for the app to be closed by the user
+        windowReturn = FindWindow(NULL,window_name);//Making sure the window was actually closed
         if(windowReturn == NULL){
             printf("App closed\n");
             windowOpened = false;
@@ -100,9 +95,8 @@ int _tmain( int argc, TCHAR *argv[] )
             printf("App continuing to run!");
         }
     }
-    printf("App Terminating\n"); 
     printf("Server Terminating\n"); 
-    TerminateProcess(server_pi.hProcess, 0);
+    TerminateProcess(server_pi.hProcess, 0);//Terminates the server downloading the videos
     
 
     // Close process and thread handles. 
