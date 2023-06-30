15f869e0daafe26f65db7044fe39ef048aa13411
Joshua Bernstein <joshuatbernstein@gmail.com>
2023-06-29 20:57:07 -0400

    settings page work

--- client/src/components/settings.js
+++ client/src/components/settings.js
@@ -1,13 +1,49 @@
-import "../App.css"
-
+import { Stack, Tabs, Tab } from "react-bootstrap";
+import { useState } from 'react'
+import "../App.css";
 
 function Settings(){
+
+    const all_menus = ['General', 'Appearance','Download Preferences'];
+    const [curMenu, setCurMenu] = useState(all_menus[0]);
+
+    function displaySettings(){
+        switch (curMenu) {
+            case "General":
+                return (<h1>General!</h1>)
+            case "Appearance":
+                return (<h1>Appearance!</h1>)
+            case "Download Preferences":
+                return (<h1>Preferences!</h1>)
+            default:
+                break;
+        }
+    }
+
     return (
-        <div className="home">
-            <h1>Settings</h1>
-            <h3>Appearance</h3>
-            <h3>Download Preferences</h3>
-            <h3></h3>
+        <div style={{paddingTop: "5rem"}}>
+            <Stack direction="horizontal" className="settings_window">
+                <Stack className="settings_tab_menu">
+                    <h2>Setting Submenus</h2>
+                    <hr/>
+                    <Tabs
+                        defaultActiveKey={all_menus[0]}
+                        id="justify-tab-example"
+                        className="flex-column"
+                        variant="pills"
+                        transition={false}
+                        onSelect={(e) => setCurMenu(e)}
+                        >
+                        {all_menus.map((el, i) => (
+                            <Tab eventKey={el} key={i} title={<h4>{el}</h4>} className="tab_button" tabClassName="tab_link"/>
+                        ))}
+                        </Tabs>
+                </Stack>
+                <Stack className="settings_options">
+                    <h1 className="center">{curMenu}</h1>
+                    {displaySettings()}
+                </Stack>
+            </Stack>
         </div>
     );
 }
