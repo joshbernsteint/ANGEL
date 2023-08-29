a2e18a8b008bb27db882649c16d89e31c378fa91
Joshua Bernstein <joshuatbernstein@gmail.com>
2023-08-29 18:38:45 -0400

    started updater

--- scripts/win_package.bat
+++ scripts/win_package.bat
@@ -1,8 +1,8 @@
 @echo off
 @REM Compile main executable
 cd ../setup
-g++ x86win_install.cpp -static -o Angelx86WinInstall
-g++ win_runner.cpp -static -o Angel 
+g++ x86win_install.cpp -static -o Angelx86WinInstall angel.res
+g++ win_runner.cpp -static -o Angel angel.res
 cd ../
 cd server && start cmd /c "pkg ./package.json" 
 cd ../electron-app && start cmd /c "npm run create"
